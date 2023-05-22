cd831aab8071d24f831327e5dd2de2636895ee5b
AndreasYager <andreas.yager@gmail.com>
2023-05-22 15:50:33 +0200

    scrolltomail

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+/*Back to top*/
+
 const topBtn = document.getElementById("topBtn");
 
 topBtn.addEventListener("click", function() {
   window.scrollTo({top: 0, behavior: 'smooth'});
+});
+
+/*Scroll to mailing list*/
+document.getElementById("mail").addEventListener("click", function() {
+  var emailElement = document.getElementById("email");
+  emailElement.scrollIntoView({behavior: "smooth"});
+  // Use setTimeout to delay focus, allowing for the scroll to happen
+  setTimeout(function(){
+      emailElement.focus();
+  }, 500);
 });
