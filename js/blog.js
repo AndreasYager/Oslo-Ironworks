018678c92644fa2ffb93f86cb67089af57f2b372
AndreasYager <andreas.yager@gmail.com>
2023-05-27 10:59:52 +0200

    Improve load times

--- js/blog.js
+++ js/blog.js
@@ -1,57 +1,39 @@
-let currentPage = 1; // keeps track of the current page
+let currentPage = 1;
 
-// Fetch posts from Wordpress
-function fetchPosts() {
+async function fetchPosts() {
     const loadingText = document.getElementById('loadingText');
-    loadingText.textContent = 'Loading...'; 
-    fetch(`https://osloironworks.andreasyager.no/wp-json/wp/v2/posts?page=${currentPage}&per_page=10`)
-        .then(response => {
-            if (!response.ok) { 
-                throw new Error('No more posts');
-            }
-            return response.json();
-        })
-        .then(posts => {
-            displayPosts(posts);
-            currentPage++; // increment the page number
-            if (posts.length < 10) {
-                document.getElementById('message').textContent = 'No more posts to load';
-            }
-            loadingText.textContent = '';
-        })
-        .catch(error => {
-            console.error(error);
-            document.getElementById('message').textContent = 'No more posts to load';
-            loadingText.textContent = '';
+    loadingText.textContent = 'Loading...';
+    try {
+        const response = await fetch(`https://osloironworks.andreasyager.no/wp-json/wp/v2/posts?page=${currentPage}&per_page=10`);
+        if (!response.ok) { 
+            throw new Error('No more posts');
+        }
+        const posts = await response.json();
+        const imagePromises = posts.map(post => fetchImage(post.featured_media));
+        const images = await Promise.all(imagePromises);
+
+        posts.forEach((post, index) => {
+            post.imageUrl = images[index];
         });
+
+        displayPosts(posts);
+        currentPage++;
+        if (posts.length < 10) {
+            document.getElementById('message').textContent = 'No more posts to load';
+        }
+        loadingText.textContent = '';
+    } catch (error) {
+        console.error(error);
+        document.getElementById('message').textContent = 'No more posts to load';
+        loadingText.textContent = '';
+    }
 }
 
-// Display posts on the webpage
 function displayPosts(posts) {
     const postsContainer = document.getElementById('postsContainer');
 
-    posts.forEach(async post => {
-        const postElement = document.createElement('div');
-        const postImage = document.createElement('img')
-        const postTitle = document.createElement('h2');
-        const postExcerpt = document.createElement('p');
-
-        postTitle.textContent = post.title.rendered;
-        postExcerpt.innerHTML = post.excerpt.rendered;
-
-        const imageUrl = await fetchImage(post.featured_media);
-        postImage.src = imageUrl;
-        postImage.alt = post.title.rendered;
-
-        postElement.appendChild(postImage);
-        postElement.appendChild(postTitle);
-        postElement.appendChild(postExcerpt);
-
-        postElement.addEventListener('click', () => {
-            window.location.href = `post.html?post=${post.id}`;
-        });
-        
-
+    posts.forEach(post => {
+        const postElement = createPostElement(post, post.imageUrl);
         postsContainer.append(postElement); 
     });
 }
@@ -62,8 +44,27 @@ async function fetchImage(mediaId) {
     return media.source_url;
 }
 
-// Call fetchPosts when the page loads
-document.addEventListener('DOMContentLoaded', fetchPosts);
+function createPostElement(post, imageUrl) {
+    const postElement = document.createElement('div');
+    const postImage = document.createElement('img');
+    const postTitle = document.createElement('h2');
+    const postExcerpt = document.createElement('p');
 
-// Load more posts when the button is clicked
+    postTitle.textContent = post.title.rendered;
+    postExcerpt.innerHTML = post.excerpt.rendered;
+    postImage.src = imageUrl;
+    postImage.alt = post.title.rendered;
+
+    postElement.appendChild(postImage);
+    postElement.appendChild(postTitle);
+    postElement.appendChild(postExcerpt);
+
+    postElement.addEventListener('click', () => {
+        window.location.href = `post.html?post=${post.id}`;
+    });
+
+    return postElement;
+}
+
+document.addEventListener('DOMContentLoaded', fetchPosts);
 document.getElementById('loadMoreBtn').addEventListener('click', fetchPosts);
--- js/index.js
+++ js/index.js
@@ -2,11 +2,19 @@ let posts = [];
 let sliderIndex = 0;
 
 async function fetchPosts() {
-    const response = await fetch(`https://osloironworks.andreasyager.no/wp-json/wp/v2/posts?page=1&per_page=100`);
+    const response = await fetch(`https://osloironworks.andreasyager.no/wp-json/wp/v2/posts?page=1&per_page=16`);
     posts = await response.json();
+    const imagePromises = posts.map(post => fetchImage(post.featured_media));
+    const images = await Promise.all(imagePromises);
+
+    posts.forEach((post, index) => {
+        post.imageUrl = images[index];
+    });
+
     displayFeaturedPost();
     displaySliderPosts();
 }
+
 
 async function fetchImage(mediaId) {
     const response = await fetch(`https://osloironworks.andreasyager.no/wp-json/wp/v2/media/${mediaId}`);
@@ -40,47 +48,48 @@ function createPostElement(post, imageUrl) {
 async function displayFeaturedPost() {
     const featuredPostContainer = document.getElementById('featuredPostContainer');
     const post = posts[0];
-    const imageUrl = await fetchImage(post.featured_media);
 
-    const postElement = createPostElement(post, imageUrl);
+    const postElement = createPostElement(post, post.imageUrl);
     featuredPostContainer.appendChild(postElement);
 }
 
 async function displaySliderPosts() {
     const sliderPostsContainer = document.getElementById('sliderPostsContainer');
-    const previousHeight = sliderPostsContainer.offsetHeight; 
-    sliderPostsContainer.style.minHeight = `${previousHeight}px`; 
+    const previousHeight = sliderPostsContainer.offsetHeight;
+    sliderPostsContainer.style.minHeight = `${previousHeight}px`;
     sliderPostsContainer.innerHTML = '';
 
     const start = sliderIndex * 3 + 1;
     const end = start + 3;
 
     for (let i = start; i < end; i++) {
-        if (i < posts.length) {
-            const post = posts[i];
-            const imageUrl = await fetchImage(post.featured_media);
-
-            const postElement = createPostElement(post, imageUrl);
-            sliderPostsContainer.appendChild(postElement);
-        }
+        const post = posts[i % posts.length];
+        const postElement = createPostElement(post, post.imageUrl);
+        sliderPostsContainer.appendChild(postElement);
     }
 
-    sliderPostsContainer.style.minHeight = '0'; 
+    sliderPostsContainer.style.minHeight = '0';
 }
+
 
 
 document.getElementById('prevBtn').addEventListener('click', () => {
     if (sliderIndex > 0) {
         sliderIndex--;
-        displaySliderPosts();
+    } else {
+        sliderIndex = Math.floor((posts.length - 1) / 3);
     }
+    displaySliderPosts();
 });
 
 document.getElementById('nextBtn').addEventListener('click', () => {
-    if (sliderIndex < (posts.length - 1) / 3) {
+    if (sliderIndex < Math.floor((posts.length - 1) / 3)) {
         sliderIndex++;
-        displaySliderPosts();
+    } else {
+        sliderIndex = 0;
     }
+    displaySliderPosts();
 });
+
 
 document.addEventListener('DOMContentLoaded', fetchPosts);
