f64cd358cb1032581f5a4b09b89426db85d1349f
AndreasYager <andreas.yager@gmail.com>
2023-05-27 15:56:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,29 +16,30 @@ document.getElementById("mail").addEventListener("click", function() {
 });
 
  /*Mail validation */
-const emailInput = document.querySelector("#email");
-const emailError = document.querySelector("#emailError");
-const successMessage = document.querySelector("#successMessage");
-const submitButton = document.querySelector("#submitBtn");
-
-submitButton.addEventListener("click", validateForm);
-
-function validateForm(event) {
-    event.preventDefault();
-
-    const email = emailInput.value;
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (emailPattern.test(email)) {
-        emailError.style.display = "none";
-        successMessage.style.display = "block";
-        successMessage.textContent = "Success! Your email was added to the mailing list.";
-        emailInput.value = "";
-    } else {
-        emailError.style.display = "block";
-        emailError.textContent = "Please enter a valid email address.";
-        successMessage.style.display = "none";
-    }
-}
+ const emailInput = document.querySelector("#footerEmail");
+ const emailError = document.querySelector("#emailError");
+ const successMessage = document.querySelector("#successMessage");
+ const submitButton = document.querySelector("#submitBtn");
+ 
+ submitButton.addEventListener("click", validateForm);
+ 
+ function validateForm(event) {
+     event.preventDefault();
+ 
+     const email = emailInput.value;
+     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+ 
+     if (emailPattern.test(email)) {
+         emailError.style.display = "none";
+         successMessage.style.display = "block";
+         successMessage.textContent = "Success! Your email was added to the mailing list.";
+         emailInput.value = "";
+     } else {
+         emailError.style.display = "block";
+         emailError.textContent = "Please enter a valid email address.";
+         successMessage.style.display = "none";
+     }
+ }
+ 
 
 
